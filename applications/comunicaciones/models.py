f09b7bad657a5e4fefdc18aac52556ac6ba35d8b
Chiara Di Giannantonio <chiaradigi2@gmail.com>
2024-05-02 11:49:09 +0200

    Adding notifications model

--- applications/comunicaciones/models.py
+++ applications/comunicaciones/models.py
@@ -1,3 +1,4 @@
+# applications/comunicaciones/models.py
 from django.db import models
 from django.contrib.auth import get_user_model
 from applications.jugador.models import Jugador
@@ -15,3 +16,15 @@ class ChatSession(models.Model):
             user1, user2 = user2, user1
         session, created = cls.objects.get_or_create(user1=user1, user2=user2)
         return session, created
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(Jugador, related_name='notifications', on_delete=models.CASCADE)
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.recipient.username} - {'Read' if self.read else 'Unread'}"
+
+def create_notification(recipient, message):
+    Notification.objects.create(recipient=recipient, message=message)
