61142759222fe550dc3a5bfec2c5749650b951b6
Chiara Di Giannantonio <chiaradigi2@gmail.com>
2024-05-15 10:50:10 +0200

    fixing css and js bug

--- web_project/settings/base.py
+++ web_project/settings/base.py
@@ -42,6 +42,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
  
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- web_project/settings/local.py
+++ web_project/settings/local.py
@@ -15,9 +15,12 @@ DATABASES = {
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
