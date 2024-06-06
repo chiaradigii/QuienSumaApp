0951c627254266dd926cc55eb0a49f86b13cc942
Chiara Di Giannantonio <chiaradigi2@gmail.com>
2024-06-06 09:14:03 +0200

    add settings prod file

---
+++ web_project/settings/prod.py
@@ -0,0 +1,33 @@
+from .base import *
+import os
+from decouple import config
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['yourdomain.com', 'your.azurewebsites.net']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# WhiteNoise to serve static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Security settings
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
